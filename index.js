f3fb6f1b0afc53f45f2e6791869f52f9b199e35a
Enric Borrallo Rodriguez <you@example.com>
2020-03-24 12:42:08 +0100

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core')
+const { GitHub, context } = require('@actions/github')
+
+const main = async () => {
+  const token = core.getInput('github-token')
+  const number = core.getInput('number')
+  const octokit = new GitHub(token)
+
+  await octokit.pulls.createReview({
+    ...context.repo,
+    pull_number: number,
+    event: 'APPROVE'
+  })
+ /*  await octokit.pulls.merge({
+    ...context.repo,
+    pull_number: number,
+  }
+  ) */
+}
+
+main().catch(err => core.setFailed(err.message))
